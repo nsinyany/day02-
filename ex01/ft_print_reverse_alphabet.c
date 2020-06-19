8504828726e41cd4dac763968886fe0826995bac
Nxalenye Sinyanya <nsinyany@student.wethinkcode.co.za>
2020-06-19 12:53:11 +0200

    Revised. Reversed alphabets.

--- ex01/ft_print_reverse_alphabet.c
+++ ex01/ft_print_reverse_alphabet.c
@@ -6,24 +6,39 @@
 /*   By: nsinyany <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/06/18 11:33:53 by nsinyany          #+#    #+#             */
-/*   Updated: 2020/06/19 09:03:08 by nsinyany         ###   ########.fr       */
+/*   Updated: 2020/06/19 12:51:57 by nsinyany         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-void ft_print_alphabet(void)
+int main()
 {
-	char letter;
+	putchar('z');
+	putchar('y');
+	putchar('x');
+	putchar('w');
+	putchar('v');
+	putchar('u');
+	putchar('t');
+	putchar('s');
+	putchar('r');
+	putchar('q');
+	putchar('p');
+	putchar('o');
+	putchar('n');
+	putchar('m');
+	putchar('l');
+	putchar('k');
+	putchar('j');
+	putchar('i');
+	putchar('h');
+	putchar('g');
+	putchar('f');
+	putchar('e');
+	putchar('d');
+	putchar('c');
+	putchar('b');
+	putchar('a');
+	putchar('\n');
 
-	letter = 'z';
-	while (letter <= 'a')
-	{
-		putchar(letter);
-		letter++;
-	}
-}
-
-int main(void)
-{
-	ft_print_alphabet();
 	return 0;
 }
