4863663ba0bd745b56e698829d3dedcd58f21f7b
Nxalenye Sinyanya <nsinyany@student.wethinkcode.co.za>
2020-06-19 12:25:30 +0200

    Last edit, character print.

--- ex00/ft_print_alphabet.c
+++ ex00/ft_print_alphabet.c
@@ -6,25 +6,40 @@
 /*   By: nsinyany <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/06/18 09:46:09 by nsinyany          #+#    #+#             */
-/*   Updated: 2020/06/18 11:09:55 by nsinyany         ###   ########.fr       */
+/*   Updated: 2020/06/19 12:05:42 by nsinyany         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <stdio.h> 
 
-void ft_print_alphabet(void)
+int main()
 {
-	char letter;
+	putchar('a');
+	putchar('b');
+	putchar('c');
+	putchar('d');
+	putchar('e');
+	putchar('f');
+	putchar('g');
+	putchar('h');
+	putchar('i');
+	putchar('j');
+	putchar('k');
+	putchar('l');
+	putchar('m');
+	putchar('n');
+	putchar('o');
+	putchar('p');
+	putchar('q');
+	putchar('r');
+	putchar('s');
+	putchar('t');
+	putchar('u');
+	putchar('v');
+	putchar('w');
+	putchar('x');
+	putchar('y');
+	putchar('z');
+	putchar('\n');
 
-	letter = 'a';
-	while (letter <= 'z')
-   	{
-		ft_putchar(letter);
-		letter++;
-	}
-}
-
-int main(void) {
-	ft_print_alphabet();
-	return 0;
 }
